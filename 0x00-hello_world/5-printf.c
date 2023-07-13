be2abda1d1025faa6686c6852a11f2ec4be14ebe
kefas emmanuel <emmakefas3@gmail.com>
2023-07-13 14:00:26 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-  *main - Using printf function to print a line
-  *return: always 0 (success)
-  */
+ * main - A printf function for a line
+ * Return: 0 (success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
