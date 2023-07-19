cc75880457a0141cdd4dee3dcdd00959b9aa63c1
kefas emmanuel <emmakefas3@gmail.com>
2023-07-18 20:53:59 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Add two values
+ * @n1 - The integer to be added
+ * @n2 - The integer to be added
+ * Return: Integer sum
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
