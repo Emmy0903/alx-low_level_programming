b3c2154523155b9c32946e06bad3c8891102d96e
kefas emmanuel <emmakefas3@gmail.com>
2023-07-19 14:42:09 -0700

    testing

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Print the first 98 Fibonacci numbers
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	unsigned long int i;
+	unsigned long int bef = 1;
+	unsigned long int aft = 2;
+	unsigned long int 
