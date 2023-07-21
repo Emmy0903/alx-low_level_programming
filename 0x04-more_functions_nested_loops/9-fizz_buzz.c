f0299ef3926ade59c9988ce7c1b5cde0039229bd
kefas emmanuel <emmakefas3@gmail.com>
2023-07-21 05:20:19 -0700

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - fizz and buzz
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0)
+			printf("Fizz");
+		else if (a % 5 == 0)
+			printf("Buzz");
+		else if (a % 15 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%a", a);
+		if (a < 100)
+			printf(' ');
+	}
+	printf('\n');
+	return (0);
+}
